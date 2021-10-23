961aa88ffe7066d5f201f11a28c0ec0b098eeef9
Nathan Johnstone <locotewz@gmail.com>
2021-10-23 08:25:53 +0200

    Uncomment deploying keda-externalscaler-hangfire

--- deploy.ps1
+++ deploy.ps1
@@ -2,5 +2,5 @@ ytt -f .\kubernetes\namespace.yaml -f .\kubernetes\values.yaml | kubectl apply -
 ytt -f .\kubernetes\keda-scaled-job-multiply.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
 ytt -f .\kubernetes\keda-scaled-job-addition.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
 ytt -f .\kubernetes\sql-server-deployment.yaml -f .\kubernetes\values.yaml | kubectl apply -f- 
-#ytt -f .\kubernetes\keda-externalscaler-hangfire.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
+ytt -f .\kubernetes\keda-externalscaler-hangfire.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
 ytt -f .\kubernetes\hangfire-dashboard.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
