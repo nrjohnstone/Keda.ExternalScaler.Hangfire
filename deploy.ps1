ec745e26c09b5b0faa3acdbf8d1817fe97259ebe
Nathan Johnstone <locotewz@gmail.com>
2021-10-23 08:23:09 +0200

    Move kubernetes deployment yamls to own folder

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,6 @@
-ytt -f .\keda-scaled-job-multiply.yaml -f .\values.yaml | kubectl apply -f-
-ytt -f .\keda-scaled-job-addition.yaml -f .\values.yaml | kubectl apply -f-
-ytt -f .\sql-server-deployment.yaml -f .\values.yaml | kubectl apply -f- 
-ytt -f .\keda-externalscaler-hangfire.yaml -f .\values.yaml | kubectl apply -f-
-ytt -f .\hangfire-dashboard.yaml -f .\values.yaml | kubectl apply -f-
+ytt -f .\kubernetes\namespace.yaml -f .\kubernetes\values.yaml | kubectl apply -f- 
+ytt -f .\kubernetes\keda-scaled-job-multiply.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
+ytt -f .\kubernetes\keda-scaled-job-addition.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
+ytt -f .\kubernetes\sql-server-deployment.yaml -f .\kubernetes\values.yaml | kubectl apply -f- 
+#ytt -f .\kubernetes\keda-externalscaler-hangfire.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
+ytt -f .\kubernetes\hangfire-dashboard.yaml -f .\kubernetes\values.yaml | kubectl apply -f-
